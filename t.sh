3920009bd573c6f0e919e11c5b1aa5297f3c6b5f
Allen Guo <guosonglin001@gmail.com>
2024-03-16 13:08:58 +0000

    wip

---
+++ t.sh
@@ -0,0 +1,14 @@
+# riscv toolchain is built with glibc
+
+mkdir -p build && pushd build
+
+RISCV=/opt/riscv_llvm_newlib
+
+CC=$RISCV/bin/riscv64-unknown-elf-gcc \
+  CXX=$RISCV/bin/riscv64-unknown-elf-g++ \
+  ../configure --host=riscv64-unknown-elf --prefix=$PWD/install
+
+bear -- make -j$(nproc)
+make install
+
+popd
